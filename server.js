85e8185b624dc8db0ea9589098b64690d0111dc4
unknown <kevinserrano0823@gmail.com>
2023-11-29 13:43:43 -0600

    added express middleware

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(htmlRoutes)
+app.use(apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
